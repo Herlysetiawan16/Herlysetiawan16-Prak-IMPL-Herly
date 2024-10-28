2c72918d76c544729a9222c6b0922df5784096ab
Herly <herlysetiawan1601@gmail.com>
2024-10-28 08:16:38 +0700

    Tambah Product, Smartphone, Vegetable

---
+++ lsp/Smartphone.java
@@ -0,0 +1,12 @@
+package lsp;
+
+class Smartphone extends Product {
+    @Override
+    String setName() {
+        return "Samsung S10+ Limited Edition";
+}
+@Override
+Date setExpiredDate() {
+return new Date(); // ???????
+}
+}
