5cdfff8e1b41a27d1d1e6dcf8cc0ab234417518a
Herly <herlysetiawan1601@gmail.com>
2024-11-03 23:12:52 +0700

    tambah Studen cinema

---
+++ Modul 2/openclose/StudentCinema.java
@@ -0,0 +1,9 @@
+StudentCinema extends Cinema { 
+    public StudentCinema(Double price) { 
+        this.price = price; 
+    } 
+    @Override 
+    Double calculateAdminFee() { 
+        return price * 2 / 100; 
+    } 
+} 
