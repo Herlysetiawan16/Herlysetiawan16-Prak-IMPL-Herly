f8eee9eb88b51bbe40409123276a6fa3bec6345e
Herly <herlyetiawan1601@gmail.com>
2025-01-05 22:59:44 +0700

    Menambahkan  Refactoring Class pada modul 9

---
+++ MODUL 9/Refactoring Class/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();  // Membuat objek person
+        person.setName("Bob Smith");    // Menggunakan setter untuk mengatur nilai name
+        System.out.print(person.getName()); // Menggunakan getter untuk mencetak nilai name
+    }
+}
